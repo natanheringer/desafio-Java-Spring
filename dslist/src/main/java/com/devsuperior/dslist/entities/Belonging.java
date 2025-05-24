34dac5d56cf5968800ebbf67dbdd552291157a34
natanheringer <natanheringer1@gmail.com>
2025-05-23 23:39:47 -0300

    Domain model

---
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,87 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+		// referencias
+		@EmbeddedId
+		private BelongingPK id;
+		
+		private Integer position;
+		
+		
+		public Belonging() {
+			
+		}
+
+
+		public Belonging(Game game, GameList list, Integer position) {
+			//referencia do game e gamelist ao belonging
+			id.setGame(game);
+			id.setList(list);
+			this.position = position;
+		
+		}
+
+
+		/**
+		 * @return the id
+		 */
+		public BelongingPK getId() {
+			return id;
+		}
+
+
+		/**
+		 * @param id the id to set
+		 */
+		public void setId(BelongingPK id) {
+			this.id = id;
+		}
+
+
+		/**
+		 * @return the position
+		 */
+		public Integer getPosition() {
+			return position;
+		}
+
+
+		/**
+		 * @param position the position to set
+		 */
+		public void setPosition(Integer position) {
+			this.position = position;
+		}
+
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id);
+		}
+
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Belonging other = (Belonging) obj;
+			return Objects.equals(id, other.id);
+		}
+		
+		
+
+}
+
--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslist.entities.Game;
 
+
+//Referencia ao tipo de identidade e ao tipo de Id
 public interface GameRepository extends JpaRepository<Game, Long>{
 
 }
