c9332e4259c7f8f8642ed0e06267ac349e8a69b9
natanheringer <natanheringer1@gmail.com>
2025-05-28 19:44:55 -0300

    Move games on list!!

--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -1,11 +1,20 @@
 package com.devsuperior.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.GameList;
 
 
 //Referencia ao tipo de identidade e ao tipo de Id
 public interface GameListRepository extends JpaRepository<GameList, Long>{
-
+	
+	// consulta sql native com o update 
+	// funçao que, dado o id da determinada lista, atualiza a posição do elemento 
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
+	
+	
 }
--- dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -10,7 +10,9 @@ import com.devsuperior.dslist.dto.GameListDTO;
 //import com.devsuperior.dslist.dto.GameMinDTO;
 //import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 // end point para achar todas as /lists 
 
@@ -20,7 +22,8 @@ public class GameListService {
 		@Autowired
 		private GameListRepository gameListRepository;
 		
-		
+		@Autowired
+		private GameRepository gameRepository;
 		
 		
 		@Transactional(readOnly = true)
@@ -30,5 +33,41 @@ public class GameListService {
 			return result.stream().map(x -> new GameListDTO(x)).toList();
 		}
 
+		@Transactional
+		public void move(Long listId, int sourceIndex, int destinationIndex) {
+			
+			List<GameMinProjection> list = gameRepository.searchByList(listId);
+			
+			// remove elemento do índice 
+			GameMinProjection obj = list.remove(sourceIndex);
+
+			//insere elemento obj em outra posiçao da lista
+			list.add(destinationIndex, obj);
+			
+			
+			//minimo e maximo entre origem e destino
+			//ternario \/\/\/\/
+			//int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex 
+			//int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex
+			int min = 0;
+			int max = 0;
+					if(sourceIndex < destinationIndex) {
+						min = sourceIndex; 
+					}else {
+						min = destinationIndex;
+					}
+					if(sourceIndex < destinationIndex) {
+						max = destinationIndex;
+					}else {
+						max = sourceIndex;
+					}
+					
+					// atualiza a posição
+					for (int i = min; i <= max; i++) {
+						gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+					}
+			
+			
+		}
 		
 }
